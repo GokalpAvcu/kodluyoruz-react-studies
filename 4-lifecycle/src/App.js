fd2e4f9c7bab2e72aacd7db8a2a93c984942e0b5
gökalp <avcu219@gmail.com>
2022-02-08 23:17:28 +0300

    add lifecycle

---
+++ 4-lifecycle/src/App.js
@@ -0,0 +1,36 @@
+
+import  { useState, useEffect } from "react";
+
+function App() {
+  const [number, setNumber] = useState(0);
+  const [name, setName] = useState("Gökalp");
+
+  useEffect (() => {
+    console.log("Component mount edildi!");
+  
+  },[]); //
+
+  useEffect(() => {
+    console.log("number state güncellendi!");
+  },[number]);
+
+  useEffect(() => {
+    console.log("name state güncellendi!");
+  },[name]);
+
+
+  return (
+    <div className="App">
+      <h1>{number}</h1>
+      <button onClick={() => setNumber(number + 1)}>Click</button>
+
+      <hr />
+
+      <h1>{name}</h1>
+      <button onClick={() => setName("Mete")}>Click</button>
+  
+    </div>
+  );
+}
+
+export default App;
