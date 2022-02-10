40317b9fcd9aad1e00ac7a009f6775f170667468
gökalp <avcu219@gmail.com>
2022-02-11 01:24:29 +0300

    add css

---
+++ 5-style-definitions/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <div style={{color: "red", backgroundColor: "white", paddingTop: 50}}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+          Cumque expedita sunt accusantium reiciendis eos impedit quis, culpa commodi, porro tempora neque deserunt fuga molestias. 
+          Dolore mollitia nihil rerum deleniti debitis.
+        </div>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
