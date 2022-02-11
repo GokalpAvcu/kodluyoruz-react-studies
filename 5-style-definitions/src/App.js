9bab9d14378d6fff75ab1c98e9d3b2c17a9f4dc2
gökalp <avcu219@gmail.com>
2022-02-11 13:05:18 +0300

    add css

--- 5-style-definitions/src/App.js
+++ 5-style-definitions/src/App.js
@@ -1,18 +1,16 @@
 import './App.css';
 
+import A from './components/A';
+import B from './components/B';
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <div style={{color: "red", backgroundColor: "white", paddingTop: 50}}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-          Cumque expedita sunt accusantium reiciendis eos impedit quis, culpa commodi, porro tempora neque deserunt fuga molestias. 
-          Dolore mollitia nihil rerum deleniti debitis.
-        </div>
+       
+       <A/>
+       <B/>
         
       </header>
     </div>
