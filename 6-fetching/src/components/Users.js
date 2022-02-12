6c36d3e127356fef6baf8e3d019082f8ef6d5d00
gökalp <avcu219@gmail.com>
2022-02-12 13:50:11 +0300

    add fetching

--- 6-fetching/src/components/Users.js
+++ 6-fetching/src/components/Users.js
@@ -1,16 +1,29 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 
 function Users() {
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
 useEffect (() => {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => console.log(res)
-    );
-}, [])
+    .then((res) => res.json())
+    .then((data) => setUsers(data))
+    .catch((e) => console.log(e))
+    .finally(() => setIsLoading(false));
+}, []);
 
 return (
-    <div>Users</div>
- )
+    <div>
+    <h1>Users</h1>
+
+    {isLoading && <div>Loading... </div>}
+
+    {
+    users.map((user) => (
+    <div key={user.id}>{user.name}</div>
+    ))}
+    </div>
+);
 }
 
-export default Users
+export default Users;
