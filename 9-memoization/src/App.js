4dbdd9ab76d8ac3ff6930fec11c03b58504b3874
gökalp <avcu219@gmail.com>
2022-02-16 10:49:32 +0300

    add memo

---
+++ 9-memoization/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import { useState } from 'react';
+
+
+function App() {
+const [number, setNumber] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>{number}</h1>
+      <button onClick={() => setNumber(number + 1)}>Click</button>
+    </div>
+  );
+}
+
+export default App;
