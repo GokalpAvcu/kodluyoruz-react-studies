531708015f84c089be6cac264f3efe1063c45870
gökalp <avcu219@gmail.com>
2022-02-16 11:16:03 +0300

    add memo

--- 9-memoization/src/App.js
+++ 9-memoization/src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 
 import { useState } from 'react';
+import  Header from './components/Header' // header componentini gösterip import ettik
 
 
 function App() {
 const [number, setNumber] = useState(0);
 
+ // header'a kosul tanimladik, number 5'den kücükse herzaman sifir gondersin degilse direkt number gondersin diye degistirdim
   return (
     <div className="App">
+      <Header number={number < 5 ? 0 : number}/>  
+
+      <hr />
+
       <h1>{number}</h1>
       <button onClick={() => setNumber(number + 1)}>Click</button>
+      
     </div>
   );
 }
