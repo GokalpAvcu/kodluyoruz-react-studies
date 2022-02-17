de17384fec33ba750c139b808a1110f6cfb5bb01
gökalp <avcu219@gmail.com>
2022-02-17 14:18:24 +0300

    add realtime

---
+++ 11-realtime-colors/client/src/components/Palatte.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Palatte() {
+  return (
+    <div className='palatte'>
+    <input type="color" />
+    <button>Click</button>
+
+
+    </div>
+  )
+}
+
+export default Palatte
